69b28d4e55866ff9ff001bc88ac19d22d2d7b447
Pavel <p.a.dudarev@mail.ru>
2022-09-20 18:46:41 +0500

    Task 41

---
+++ Task041/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите числа! ");
+string numbers = Console.ReadLine()!;
+string[] SplitedNumbers = numbers.Split(' ');
+int[] ParseNumbers = new int[SplitedNumbers.Length];
+int count = 0;
+
+for (int i = 0; i < SplitedNumbers.Length; i++)
+{
+    ParseNumbers[i] = int.Parse(SplitedNumbers[i]);
+    if(ParseNumbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"{count} - количество положительных чисел");
